de496e06f3123358152cc4a7191ca6157ee737cc
Dan Nai <Daniel2.Nai@live.uwe.ac.uk>
2024-04-25 13:32:32 +0100

    Whale Animation done

---
+++ Assets/Animator/Whale/WhaleDeath.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleDeath : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ShowUI();
+        }
+    }
+
+    public void HideUI()
+    {
+        ui.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    public void ShowUI()
+    {
+        ui.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Death()
+    {
+        animator.SetTrigger("Death");
+    }
+
+    public void PressYes()
+    {
+        HideUI();
+        Death();
+    }
+
+    public void PressNo()
+    {
+        HideUI();
+
+    }
+
+}
---
+++ Assets/Scripts/WhaleDive.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleDive : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ShowUI();
+        }
+    }
+
+    public void HideUI()
+    {
+        ui.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    public void ShowUI()
+    {
+        ui.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Dive()
+    {
+        animator.SetTrigger("Dive");
+    }
+
+    public void PressYes()
+    {
+        HideUI();
+        Dive();
+    }
+
+    public void PressNo()
+    {
+        HideUI();
+
+    }
+
+}
